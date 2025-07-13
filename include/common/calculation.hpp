2caddf1e8e1fa9c3ac4b21f1fc6554dc3d2b47e4
hyheiyue <heiyuemoon@outlook.com>
2025-07-14 00:04:26 +0800

    x

--- include/common/calculation.hpp
+++ include/common/calculation.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "type/type.hpp"
 
-double orientationToYaw(const Eigen::Quaterniond& q);
+double orientationToYaw(const tf::Quaternion& orientation);
 void command_callback(Armors& armors);
 void ex(double& a, double& min, double& max);
 void command_callbackypd(const Armors& armors);
