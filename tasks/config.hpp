b76cd5fc8332c5ebb4a831b70e9af77074c010ae
hyheiyue <heiyuemoon@outlook.com>
2026-01-20 17:07:34 +0800

    small update

---
+++ tasks/config.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#define OPENVINO_CONFIG "config/detect_openvino.yaml"
+#define TENSORRT_CONFIG "config/detect_trt.yaml"
+#define NCNN_CONFIG "config/detect_ncnn.yaml"
+#define ONNXRUNTIME_CONFIG "config/detect_ort.yaml"
+#define OPENCV_CONFIG "config/detect_opencv.yaml"
+#define COMMON_CONFIG "config/common.yaml"
+#define CAMERA_CONFIG "config/camera.yaml"
+#define AUTO_AIM_CONFIG "config/auto_aim.yaml"
+#define AUTO_BUFF_CONFIG "config/auto_buff.yaml"
+#define AUTO_GUIDANCE_CONFIG "config/auto_guidance.yaml"
--- tasks/type_common.hpp
+++ tasks/type_common.hpp
@@ -6,17 +6,8 @@
 #include <optional>
 #include <shared_mutex>
 #include <yaml-cpp/yaml.h>
+#include "config.hpp"
 
-#define OPENVINO_CONFIG "config/detect_openvino.yaml"
-#define TENSORRT_CONFIG "config/detect_trt.yaml"
-#define NCNN_CONFIG "config/detect_ncnn.yaml"
-#define ONNXRUNTIME_CONFIG "config/detect_ort.yaml"
-#define OPENCV_CONFIG "config/detect_opencv.yaml"
-#define COMMON_CONFIG "config/common.yaml"
-#define CAMERA_CONFIG "config/camera.yaml"
-#define AUTO_AIM_CONFIG "config/auto_aim.yaml"
-#define AUTO_BUFF_CONFIG "config/auto_buff.yaml"
-#define AUTO_GUIDANCE_CONFIG "config/auto_guidance.yaml"
 struct CommonFrame {
     cv::Mat src_img;
     std::chrono::steady_clock::time_point timestamp;
